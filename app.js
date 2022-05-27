d3e86103ad70560ab429fe12f6f208e9322897a5
aribrilliantsyah <ariardiansyah101@gmail.com>
2022-05-27 22:11:46 +0700

    [UPDATE]

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const swaggerDocument = YAML.load('collection.yaml')
 //Setup Log
 const originalSend = app.response.send
 
-let log_name = './logs/access_log_'+moment().format('YYYY_MM_DD')+'.log';
+let log_name = '/logs/access_log_'+moment().format('YYYY_MM_DD')+'.log';
 let accessLogStream = fs.createWriteStream(path.join(__dirname, log_name), { flags: 'a' })
 
 app.response.send = function sendOverWrite(body) {
@@ -54,6 +54,10 @@ morgan.token('req-body', (req, res) => {
 
 app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] | @req :req-body => @res :res-body', {
     stream: accessLogStream
+}))
+
+app.use(morgan(':remote-addr - :remote-user [:date[clf]] ":method :url HTTP/:http-version" :status :res[content-length] | @req :req-body => @res :res-body', {
+    // stream: accessLogStream
 }))
 
 app.set('view engine', 'ejs')
